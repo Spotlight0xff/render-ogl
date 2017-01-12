319cf4b153b79b09a3f6c338d8faeaabe87df1db
Spotlight0xff <spotlight0xff@gmail.com>
2017-01-12 02:20:42 +0100

    enabled walking around with WASD

--- src/main.cpp
+++ src/main.cpp
@@ -58,18 +58,17 @@ int main() {
   Input input(window);
 
 
-
-  Shader shader("resources/shaders/model_vertex.glsl", "resources/shaders/default_fragment.glsl");
-  Mesh mesh("resources/models/cube.obj", shader);
+  Shader shader("resources/shaders/model_vertex.glsl", "resources/shaders/model_fragment.glsl");
+  Mesh mesh("resources/models/dragon.obj", shader);
 
 
 
   // Setup model-view-projection
-  glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -4.0));
-  glm::mat4 view = glm::lookAt(glm::vec3(0.0, 2.0, 0.0), glm::vec3(0.0, 0.0, -4.0), glm::vec3(0.0, 1.0, 0.0));
-  glm::mat4 projection = glm::perspective(45.0f, 1.0f*WINDOW_WIDTH/WINDOW_HEIGHT, 0.1f, 10.0f);
+  //glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -4.0));
+  //glm::mat4 view = glm::lookAt(glm::vec3(0.0, 2.0, 0.0), glm::vec3(0.0, 0.0, -4.0), glm::vec3(0.0, 1.0, 0.0));
+  //glm::mat4 projection = glm::perspective(45.0f, 1.0f*WINDOW_WIDTH/WINDOW_HEIGHT, 0.1f, 10.0f);
 
-  glm::mat4 mvp = projection * view * model;
+  //glm::mat4 mvp = projection * view * model;
 
   // disable vsync
   glfwSwapInterval(0);
@@ -82,48 +81,80 @@ int main() {
 
   glfwSetInputMode(window, GLFW_STICKY_KEYS, GL_TRUE);
 
-  double lastTime = glfwGetTime();
+  glm::vec3 camera_pos = glm::vec3(0.0f, 2.0f, 0.0f);
+  glm::vec3 camera_right = glm::vec3(1.0f, 0.0f, 0.0f);
+  glm::vec3 camera_front = glm::vec3(0.0f, 0.0f, -1.0f);
+  glm::vec3 camera_up = glm::vec3(0.0f, 1.0f, 0.0f);
+  GLfloat pre_camera_speed = 5.0f;
+
+  GLfloat delta_frame = 0.0;
+  GLfloat last_frame = 0.0;
+  GLfloat current_time = GLfloat(glfwGetTime());
+  GLfloat last_time = GLfloat(glfwGetTime());
   std::string performance_str;
-  std::string fps_str;
+  std::string fps_str("1337.0 FPS");
   int n_frames = 0;
   do {
     glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     // Measure speed
-    double currentTime = glfwGetTime();
+    current_time = GLfloat(glfwGetTime());
+    delta_frame = current_time - last_frame;
+    last_frame = current_time;
     n_frames ++;
 
-    // Set vertices color
-    //GLfloat shift_x = (sin(currentTime) / 2);
-    //GLfloat shift_y = (cos(currentTime) / 2);
-    //GLint offset_loc = glGetUniformLocation(shader.getId(), "vec_offset");
-
-    if (currentTime - lastTime >= 1.0) {
-      double frame_ms = 1000.0/double(n_frames);
-      double fps = n_frames;
+    if (current_time - last_time >= 1.0f) {
+      GLfloat frame_ms = 1000.0f/GLfloat(n_frames);
+      GLfloat fps = n_frames;
       std::cout << frame_ms << " ms/frame\n";
 
       n_frames = 0;
-      lastTime += 1.0;
+      last_time += 1.0;
       performance_str = std::to_string(frame_ms) + " ms/frame";
       fps_str = std::to_string(fps) + " FPS";
     }
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     fontRenderer.render(performance_str, 20.0f,WINDOW_HEIGHT - 50.0f, glm::vec3(0.5,0.8f, 0.2f));
     fontRenderer.render(fps_str, 20.0f,WINDOW_HEIGHT - 120.0f, glm::vec3(0.5,0.8f, 0.2f));
 
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
     // Rendering here
     mesh.draw();
+
+
+    glm::mat4 model = glm::translate(glm::mat4(1.0f), glm::vec3(0.0, 0.0, -20.0));
+    glm::mat4 view = glm::lookAt(camera_pos, camera_pos + camera_front, camera_up);
+    glm::mat4 projection = glm::perspective(45.0f, 1.0f*WINDOW_WIDTH/WINDOW_HEIGHT, 0.1f, 100.0f);
+
+    glm::mat4 mvp = projection * view * model;
     glUniformMatrix4fv(glGetUniformLocation(shader.getId(), "mvp"), 1, GL_FALSE, glm::value_ptr(mvp));
 
 
     glfwSwapBuffers(window);
     glfwPollEvents();
-    input.handle([](bool keys[]) {
-        if (keys[GLFW_KEY_W]) {
-          std::cout << ".";
-        }
-        });
+    input.handleKeys([&camera_pos, &pre_camera_speed,
+                     &camera_front, &camera_up, &camera_right,
+                     &delta_frame](bool keys[]) {
+     GLfloat camera_speed = pre_camera_speed * delta_frame;
+     if (keys[GLFW_KEY_W]) {
+       camera_pos += camera_speed * camera_front;
+     }
+     if (keys[GLFW_KEY_S]) {
+       camera_pos -= camera_speed * camera_front;
+     }
+     if (keys[GLFW_KEY_A]) {
+       //glm::vec3 camera_right = glm::normalize(glm::cross(camera_front, camera_up));
+       camera_pos -= camera_right * camera_speed;
+     }
+     if (keys[GLFW_KEY_D]) {
+       //glm::vec3 camera_right = glm::normalize(glm::cross(camera_front, camera_up));
+       camera_pos += camera_right * camera_speed;
+     }
+     if (keys[GLFW_KEY_SPACE]) {
+       camera_pos += camera_speed * camera_up;
+     }
+   });
   } while(glfwWindowShouldClose(window) == 0);
 
   glfwTerminate();
