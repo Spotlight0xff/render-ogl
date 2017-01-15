0f5b50f2a017303113bb25d8fdc2b84883434986
Spotlight0xff <spotlight0xff@gmail.com>
2017-01-15 16:06:05 +0100

    added unit testing framework

--- src/Util.h
+++ src/Util.h
@@ -18,7 +18,7 @@ void APIENTRY glDebugOutput(GLenum source,
                             GLenum severity,
                             GLsizei /*length*/,
                             const GLchar *message,
-                            void * /*userParam*/);
+                            const void * /*userParam*/);
 
 bool enableDebugOutput();
 
