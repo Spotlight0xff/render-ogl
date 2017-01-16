fa1fd13fbd3b497c06065b618e1f58d742344325
Spotlight0xff <spotlight0xff@gmail.com>
2017-01-16 10:12:59 +0100

    added negative shader compile test

--- tests/engine/Shader.cpp
+++ tests/engine/Shader.cpp
@@ -15,7 +15,7 @@ using ::testing::SetArgPointee;
 
 namespace engine {
 
-TEST(Shader, Loading) {
+TEST(Shader, Load) {
   GLMock mock;
   ON_CALL(mock, gl_GetShaderiv(_, _, _))
           .WillByDefault(SetArgPointee<2>(1));
@@ -32,9 +32,36 @@ TEST(Shader, Loading) {
   EXPECT_CALL(mock, gl_AttachShader(_, _)).Times(2);
   EXPECT_CALL(mock, gl_DeleteShader(_)).Times(2);
   EXPECT_CALL(mock, gl_GetProgramiv(_, _, _)).Times(1);
-
   Shader shader("light_simple");
 }
 
+
+TEST(Shader, NegativeLoad) {
+  GLMock mock;
+
+  // Fail GetShaderiv
+  ON_CALL(mock, gl_GetShaderiv(_, _, _))
+          .WillByDefault(SetArgPointee<2>(0));
+
+  // vertex  shader compilation
+  EXPECT_CALL(mock, gl_CreateShader(_)).Times(1);
+  EXPECT_CALL(mock, gl_ShaderSource(_, _, _, _)).Times(1);
+  EXPECT_CALL(mock, gl_CompileShader(_)).Times(1);
+  EXPECT_CALL(mock, gl_GetShaderiv(_, _, _)).Times(0);
+  EXPECT_CALL(mock, gl_GetShaderInfoLog(_, _, _, _)).Times(1);
+
+
+  // linking process
+  EXPECT_CALL(mock, gl_CreateProgram()).Times(0);
+  EXPECT_CALL(mock, gl_LinkProgram(_)).Times(0);
+  EXPECT_CALL(mock, gl_AttachShader(_, _)).Times(0);
+  EXPECT_CALL(mock, gl_DeleteShader(_)).Times(0);
+  EXPECT_CALL(mock, gl_GetProgramiv(_, _, _)).Times(0);
+
+  EXPECT_CALL(mock, gl_GetShaderiv(_, _, _)).Times(1);
+
+
+  Shader shader("light_simple");
+}
 
 } // end namespace engine
