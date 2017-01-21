894b504e635fe3480f571e7c05ce29cf839864c7
Spotlight0xff <spotlight0xff@gmail.com>
2017-01-21 01:23:50 +0100

    replace commands in cfg with scripts, enable caching

---
+++ scripts/install_glew.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+VERSION="2.0.0"
+
+mkdir -p build && cd build
+
+# check if its installed already
+installed=$(pkg-config --modversion glew)
+if [[ "$VERSION" == "$installed" ]]; then
+  echo "GLEW at version ${VERSION} already installed."
+  exit 0
+fi
+
+wget https://github.com/nigels-com/glew/releases/download/glew-${VERSION}/glew-${VERSION}.tgz
+tar -xzvf glew-${VERSION}.tgz
+pushd glew-${VERSION}
+make
+sudo make install
+popd
+#rm -rf glew-${VERSION}.tgz glew-${VERSION}
+1
---
+++ scripts/install_glfw.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+VERSION="3.2.1"
+
+# check if its installed already
+installed=$(pkg-config --modversion glfw3)
+if [[ "$VERSION" == "$installed" ]]; then
+  echo "GLFW at version ${VERSION} already installed."
+  exit 0
+fi
+
+mkdir -p build && cd build
+
+wget -nc https://github.com/glfw/glfw/archive/${VERSION}.tar.gz
+tar -xzvf ${VERSION}.tar.gz
+pushd glfw-${VERSION}
+cmake -G "Unix Makefiles" .
+make
+sudo make install
+popd
+
+#rm -rf glfw-${VERSION} ${VERSION}.tar.gz
+1
