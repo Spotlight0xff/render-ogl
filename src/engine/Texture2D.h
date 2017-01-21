02a5995b93c993a038412e0c9e8827212afb18b5
Spotlight0xff <spotlight0xff@gmail.com>
2017-01-21 14:15:18 +0100

    add todo to texture loading

--- src/engine/Texture2D.h
+++ src/engine/Texture2D.h
@@ -28,6 +28,7 @@ class Texture2D : public Texture {
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
+        // TODO: support more colorspaces than RGB
         glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width_, height_,
                      0, GL_RGB, GL_UNSIGNED_BYTE, image);
         glGenerateMipmap(GL_TEXTURE_2D);
